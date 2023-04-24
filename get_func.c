4dc3b6b5ef5520f00d5476cee2586deda3faa49f
Anzelah <jepkoechanzellah@gmail.com>
2023-04-24 14:55:51 -0700

    check for null terminator

--- get_func.c
+++ get_func.c
@@ -36,10 +36,14 @@ int get_func(const char *format, print f_arr[], va_list arg_list)
 				break;
 				}
 			}
-			if (f_arr[j].my_printf == NULL && format[i + 1] != ' ')
-			{
-				write_char(format[i]);
-				printed_chars++;
+			if (f_arr[j].my_printf == NULL)
+				if (format[i + 1] != '\0')
+				{
+					write_char(format[i]);
+					printed_chars++;
+				}
+				else 
+					return (-1);
 
 			}
 
