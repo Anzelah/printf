be43110e0b2ad8c661f18d4a40d6b6f346839cd9
Anzelah <jepkoechanzellah@gmail.com>
2023-04-25 15:07:53 -0700

    changes to file

--- get_func.c
+++ get_func.c
@@ -33,11 +33,16 @@ int get_func(const char *format, print f_arr[], va_list arg_list)
 				break;
 				}
 			}
-			if (f_arr[j].my_printf == NULL && format[i + 1] != '\0')
+			if (f_arr[j].my_printf == NULL)
 			{
-				write_char(format[i]);
-				write_char(format[i + 1]);
-				printed_chars += 2;
+				if (format[i + 1] != '\0')
+				{
+					write_char(format[i]);
+					write_char(format[i + 1]);
+					printed_chars += 2;
+				}
+				else
+					return (-1);
 			}
 			i++;
 		}
