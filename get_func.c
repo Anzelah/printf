a98a50a04aaf3e1c0d818aa910783ae19b4cc5e5
Anzelah <jepkoechanzellah@gmail.com>
2023-04-25 16:56:29 -0700

    check for null string

--- get_func.c
+++ get_func.c
@@ -34,11 +34,14 @@ int get_func(const char *format, print f_arr[], va_list arg_list)
 				break;
 				}
 			}
-			if (f_arr[j].my_printf == NULL)
+			if (f_arr[j].my_printf == NULL && format[i + 1] != '\0')
 			{
 				write_char(format[i]);
-				printed_chars++;
+				write_char(format[i + 1]);
+				printed_chars += 2;
 			}
+			else
+				return (-1);
 		}
 		else /* char isn't % */
 		{
